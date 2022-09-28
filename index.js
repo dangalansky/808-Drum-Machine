aa7282c1ba3792f1e27a83867c04f0eb110b3ea2
Vit0rg <vgois032@gmail.com>
2022-09-28 20:27:34 -0300

    Added dark mode + CSS fixes

--- index.js
+++ index.js
@@ -1,62 +1,74 @@
-$("button").click(function() {
+$("#menu").click(function () 
+{
+  const body = $("body")
 
-  var buttonText = $(this).attr("class")[0];
+  $(this).attr("src").includes("dark") ?
+  $(this).attr("src", "./images/light.svg")
+  :
+  $(this).attr("src", "./images/dark.svg")
+
+  body.toggleClass("dark-mode")
+})
+
+$("button").click(function() 
+{
+  let buttonText = $(this).attr("class")[0];
   playSound(buttonText);
   buttonPressed(buttonText);
-
 });
 
-$(document).keydown(function(event) {
-
+$(document).keydown(function(event) 
+{
   playSound(event.key);
   buttonPressed(event.key);
-
 });
 
-function playSound(key) {
-
-  switch (key) {
-
+function playSound(key) 
+{
+  switch (key) 
+  {
     case "a":
-      var kick = new Audio('sounds/kick.mp3');
+      let kick = new Audio('sounds/kick.mp3');
       kick.play();
       break;
-
+      
     case "s":
-      var snare = new Audio('sounds/snare.mp3');
+      let snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
     case "d":
-      var hihat = new Audio('sounds/hihat.mp3');
+      let hihat = new Audio('sounds/hihat.mp3');
       hihat.play();
       break;
 
     case "j":
-      var clap = new Audio('sounds/clap.mp3');
+      let clap = new Audio('sounds/clap.mp3');
       clap.play();
       break;
 
     case "k":
-      var clink = new Audio('sounds/clink.mp3');
+      let clink = new Audio('sounds/clink.mp3');
       clink.play();
       break;
 
     case "l":
-      var cymbal = new Audio('sounds/cymbal.mp3');
+      let cymbal = new Audio('sounds/cymbal.mp3');
       cymbal.play();
       break;
-
-    default:
-      console.log(key);
-
   }
 }
 
-function buttonPressed(key) {
-  var activeButton = document.querySelector("." + key);
-  activeButton.classList.add("pressed");
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 100);
+function buttonPressed(key) 
+{
+  let activeButton = document.querySelector("." + key);
+  let validButtons = ['a', 's', 'd', 'j', 'k', 'l']
+  
+  if (validButtons.includes(key))
+    activeButton.classList.add("pressed");
+  setTimeout(function() 
+  {
+    if (validButtons.includes(key))
+      activeButton.classList.remove("pressed");
+  }, 150);
 }
